0642bbdb0543152f7dc5835318a7f8b9161b07c4
Ujjwaltyagi <ujjwaltyagii603@gmail.com>
2025-11-18 20:40:49 +0530

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,2 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  basePath: "/portfolio", 
-  images: {
-    unoptimized: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-};
-
-export default nextConfig;
+const nextConfig = {};
