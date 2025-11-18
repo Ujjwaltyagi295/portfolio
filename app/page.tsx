3c5c5ff80043f087e3e684c7f2eff7969729e591
Ujjwaltyagi <ujjwaltyagii603@gmail.com>
2025-11-18 20:26:09 +0530

    fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 
 import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { useState } from "react";
 import { IoCopyOutline } from "react-icons/io5";
 // Also install this npm i --save-dev @types/react-lottie
