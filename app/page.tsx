c1daee1018563672995edc8bbca70d9f222e66d1
Ujjwaltyagi <ujjwaltyagii603@gmail.com>
2025-11-18 20:13:22 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import Hero from "@/components/Hero";
+import Grid from "@/components/Grid";
+import Approach from "@/components/Approach";
+import Experience from "@/components/Experience";
+import RecentProjects from "@/components/RecentProjects";
+
+const Home = () => {
+  return (
+    <main className="relative bg-black flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <Hero />
+        <Grid />
+        <RecentProjects />
+
+        <Experience />
+        <Approach />
+      </div>
+    </main>
+  );
+};
+
+export default Home;
---
+++ data/index.ts
@@ -0,0 +1,189 @@
+export const navItems = [
+  { name: "About", link: "/#about" },
+  { name: "Projects", link: "/#projects" },
+
+  { name: "Contact", link: "/#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building An AI Internship App ",
+    description: "Quark",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Velour fashion landing page",
+    des: "Created a landing page of clothing brand",
+    img: "/try.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "Prolink - team and project finding app ",
+    des: "ProLink is a platform built for developers to find or create teams and collaborate on real-world projects through a project management workspace.",
+    img: "/prolink.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "https://pro-link-jf3b.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "Delhicare hospital web",
+    des: "DelhiCare is a healthcare platform that enables users to book hospital beds, access an OPD (Outpatient Department) system, ",
+    img: "/delhicare.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "https://delhicare.vercel.app/",
+  },
+  {
+    id: 4,
+    title: "Bakery website landing page",
+    des: "Created a landing page for bakery website",
+    img: "/bakery.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "https://bakery-umber-one.vercel.app/",
+  },
+];
+
+
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Fullstack developer (current)",
+    desc: "I build real products with a focus on clean, scalable architecture backend-first, full-stack by default. I work across the stack using technologies like Next.js, PostgreSQL, TypeScript, and Node.js.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Application Developer",
+    desc: "Designed and developed mobile applications for iOS, Android, Windows and other OS platforms using React Native and Flutter.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Software Developer",
+    desc: "Did Software Development with Python, C, C++, C# and Java for Windows and mobile platforms.",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "A beginner developer",
+    desc: "Developed some extremely small projects to build better understanding in the field.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/ujjwaltyagi295",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link: "https://twitter.com/ujjwaltyagi295",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://linkedin.com",
+  },
+];
